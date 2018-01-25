10d82de03e6fb690f4f16cb4e62423ca16347300
Anan Lappikulthong <ananlappikulthong@Anans-MacBook-Air.local>
2018-01-25 13:56:10 +0700

    3

---
+++ button.swift
@@ -0,0 +1,17 @@
+//
+//  button.swift
+//  LMWH Dose
+//
+//  Created by Anan Lappikulthong on 1/25/18.
+//  Copyright © 2018 Anan Lappikulthong. All rights reserved.
+//
+
+import UIKit
+
+class button: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+    }
+
+}
